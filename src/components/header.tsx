ff92ed8a2fc577e1c49630f233ec999ada758f04
Dhruvil Dave <dhruvil.dave@outlook.com>
2021-03-07 15:49:06 +0530

    Gatsby Tailwind setup

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type HeaderProps = {
+  headerText: string
+}
+
+export default function Header(props: HeaderProps) {
+  return <h1 className='m-2 font-bold'>{props.headerText}</h1>
+}
