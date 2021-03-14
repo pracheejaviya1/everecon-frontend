c102680b4350121acad61bac2073d9837012bed9
pracheejaviya1 <prachee.j@ahduni.edu.in>
2021-03-15 01:43:28 +0530

    image not rendering

---
+++ src/components/landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Landing() {
+  return (
+    <div style={{ height: '100px', width: '100px' }}>
+      <img src='./assets/Images/default.jpg' alt='piggy plushie' />
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from '../components/login';
+import Landing from '../components/landing';
 
 export default function Home() {
-  return <Login></Login>;
+  return <Landing></Landing>;
 }
