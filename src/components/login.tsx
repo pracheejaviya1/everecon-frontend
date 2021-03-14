b929b15577d4cdd2ea0c9aa66e6c58a8baef64fe
pracheejaviya1 <prachee.j@ahduni.edu.in>
2021-03-15 01:22:48 +0530

    login component done

---
+++ src/components/login.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <h1>EveRecon</h1>
+      <h2>Welcome!</h2>
+      <text>Sign In</text>
+      <form>
+        <label>
+          Username
+          <input type='email' name='email' />
+        </label>
+        <br />
+        <label>
+          Password
+          <input type='text' name='name' />
+        </label>
+      </form>
+      <hr />
+      <text>OR</text>
+      <hr />
+      <br />
+      <button>
+        <text> Continue with Google </text>
+      </button>
+      <br />
+      <button>
+        <text> Continue with Facebook </text>
+      </button>
+      <br />
+      <text>
+        Forgot password? <text>Reset</text>
+      </text>
+      <br />
+      <button>
+        <text> Login </text>
+      </button>
+      <br />
+      <button>
+        <text> Create account </text>
+      </button>
+      <br />
+    </div>
+  );
+}
