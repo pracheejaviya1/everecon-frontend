8199a6ea0db61932b9988f646913edbb4ac14b8d
pracheejaviya1 <prachee.j@ahduni.edu.in>
2021-04-08 02:35:58 +0530

    Community page 2 component done

---
+++ src/components/community/CreateCommunityPage2.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from '../header';
+import communityImage from '../../assets/Images/default.jpg';
+
+export default function createCommunityOne() {
+  return (
+    <div>
+      <Header />
+      {/*Add back icon */}
+      <text>Create Community</text>
+      <hr />
+      <div>
+        <text>Add Members</text>
+        <form className='w-1/2'>
+          <input name='Enter member name' />
+          {/*Add search icon here */}
+        </form>
+        {/*Maybe move this into card */}
+        <div>
+          <img src={communityImage} />
+          <div>
+            <text> Name</text>
+            <text>Location</text>
+          </div>
+          {/*Minus icon */}
+        </div>
+      </div>
+      <button>Next</button>
+    </div>
+  );
+}
