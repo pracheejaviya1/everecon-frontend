2201712e8f7d2d858ba5f970cc1dc47a01278b4c
pracheejaviya1 <prachee.j@ahduni.edu.in>
2021-04-17 18:04:41 +0530

    create event pages done

--- src/components/event/createEventPage1.tsx
+++ src/components/event/createEventPage1.tsx
@@ -7,7 +7,7 @@ export default function CreateEventOne() {
     <div className='h-screen'>
       <Header />
       {/*Add back icon */}
-      <div className='flex flex-col h-5/6 my-8 justify-center items-center'>
+      <div className='flex flex-col h-5/6 justify-center items-center'>
         <div className='flex items-center justify-between border-b-2 pb-2 w-1/2'>
           <svg
             xmlns='https://www.w3.org/2000/svg'
@@ -23,38 +23,45 @@ export default function CreateEventOne() {
               d='M10 19l-7-7m0 0l7-7m-7 7h18'
             />
           </svg>
-          <span className='text-4xl flex-grow text-center'>Create Event</span>
+          <span className='text-4xl flex-grow text-center font-mulish'>
+            Create Event
+          </span>
         </div>
         <figure className='mt-8 mb-6'>
           <img className='h-60 w-80 rounded-lg' src={EventImg} />
-          <figcaption className='py-2 text-center'>
+          <figcaption className='py-2 text-center font-mulish'>
             Upload Event photo
           </figcaption>
         </figure>
         <form className='flex flex-col'>
           <label className='my-2' htmlFor='Community name'>
             <input
-              className='border border-black p-2 rounded-lg'
+              className='border border-gray-400 p-2 w-80 rounded-lg font-roboto text-sm'
               placeholder='Community name'
               name='Community name'
             />
           </label>
           <label className='my-2' htmlFor='email id'>
             <input
-              className='border border-black p-2 rounded-lg'
+              className='border border-gray-400 p-2 w-80 rounded-lg font-roboto text-sm'
               placeholder='email id'
               name='email id'
             />
           </label>
           <label className='my-2' htmlFor='Description'>
             <input
-              className='border border-black p-2 rounded-lg'
+              className='border border-gray-400 p-2 w-80 h-40 rounded-lg font-roboto text-sm'
               placeholder='Description'
               name='Description'
             />
           </label>
         </form>
-        <button type='submit'>Next</button>
+        <button
+          className='text-white text-sm bg-blue-400 py-2 px-4 rounded-lg font-inter'
+          onClick={e => e.preventDefault()}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
