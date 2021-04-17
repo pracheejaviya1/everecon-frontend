25f7d765621d3797b2685f231105fe5e1943ede8
Dhruvil Dave <dhruvil.dave@outlook.com>
2021-04-17 18:09:44 +0530

    Community Landing

---
+++ src/components/community/landing.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import communityImg from '../../assets/Images/community.jpg';
+import Header from '../header';
+
+type TagProps = {
+  title: string;
+};
+
+function Tag(props: TagProps) {
+  return (
+    <span className='rounded-xl text-center m-2 bg-gray-300 px-2 py-1'>
+      {props.title}
+    </span>
+  );
+}
+
+export default function Landing() {
+  return (
+    <div className='h-screen'>
+      <Header />
+      <div className='flex border-b-2 py-10 w-2/3 mx-auto'>
+        <img className='rounded-md' src={communityImg} />
+        <div className='flex ml-10 items-start justify-between h-full flex-col'>
+          <h1 className='font-bold text-2xl mx-2'>NAME</h1>
+          <p className='text-2xl mx-2 mb-1'>Location</p>
+          <p className='text-2xl mx-2 text-gray-400 mb-1'>Lead</p>
+          <button
+            className='text-lg bg-blue-500 text-white rounded-md px-2 py-1 mx-2 my-1'
+            onClick={e => e.preventDefault()}
+            type='submit'
+          >
+            Follow
+          </button>
+        </div>
+      </div>
+      <div className='w-2/3 m-auto py-4 font-inter justify-between flex'>
+        <div className='w-1/3'>
+          <div className='w-full flex justify-between'>
+            <button onClick={e => e.preventDefault()} type='submit'>
+              About
+            </button>
+            <button onClick={e => e.preventDefault()} type='submit'>
+              Events
+            </button>
+            <button onClick={e => e.preventDefault()} type='submit'>
+              Members
+            </button>
+            <button onClick={e => e.preventDefault()} type='submit'>
+              Contact
+            </button>
+          </div>
+          <p className='my-10 text-gray-500'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            vitae, ea nisi nulla iusto aperiam ipsam asperiores enim harum
+            doloribus atque adipisci cupiditate id quis fugiat culpa voluptates,
+            porro reprehenderit!
+          </p>
+        </div>
+        <div className='flex items-center flex-col w-1/4'>
+          <h3 className='text-center'>Tags</h3>
+          <div className='grid items-center my-10 grid-cols-3'>
+            <Tag title='Tag' />
+            <Tag title='Tag' />
+            <Tag title='Tag' />
+            <Tag title='Tag' />
+            <Tag title='Tag' />
+            <Tag title='Tag' />
+          </div>
+          {/* <button
+            className='bg-blue-500 rounded-md my-5 px-2 py-1 text-white'
+            onClick={e => e.preventDefault()}
+            type='submit'
+          >
+            Join Community
+          </button> */}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import CreateCommunityOne from '../components/community/createCommunityPage1';
 import CreateCommunityTwo from '../components/community/CreateCommunityPage2';
 import CreateCommunityThree from '../components/community/createCommunityPage3';
+import Landing from '../components/community/landing';
 import CreateEventOne from '../components/event/createEventPage1';
 import CreateEventTwo from '../components/event/createEventPage2';
 import UpdateEventTwo from '../components/event/UpdateEvent2';
@@ -10,10 +11,6 @@ import UpdateEventTwo from '../components/event/UpdateEvent2';
 export default function Home() {
   return (
     <div className='divide-y'>
-      {/* <Communities />
-      <ExploreEvents /> */}
-      {/* <ViewEvent />
-      <EventRegister title='Talking Tech with Ingenium' datetime={new Date()} /> */}
       <CreateCommunityOne />
       <CreateCommunityTwo />
       <CreateCommunityThree />
@@ -22,6 +19,7 @@ export default function Home() {
       <UpdateEventTwo details='Lorem Ipsum' />
       <Link to='/Settings/SettingsAccount'>Settings</Link>
       <Link to='/Settings/SettingsSecurity'>Setting</Link>
+      <Landing />
     </div>
   );
 }
