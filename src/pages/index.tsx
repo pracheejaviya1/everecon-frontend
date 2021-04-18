bc01e8783391fbae1ac5fbe0a7b5e7ab73e509a9
Dhruvil Dave <dhruvil.dave@outlook.com>
2021-04-18 21:40:21 +0530

    Dropdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import Dropdown from '../components/dropdown';
 import Signin from './Signin/signin';
 
 export default function Home() {
   return (
     <div className='w-screen'>
       <Signin />
+      <Dropdown />
     </div>
   );
 }
