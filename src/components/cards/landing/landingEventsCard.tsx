28e01bf8a2150940adef223ff08774495b5a5d9b
Dhruvil Dave <dhruvil.dave@outlook.com>
2021-04-28 16:55:21 +0530

    Event

--- src/components/cards/landing/landingEventsCard.tsx
+++ src/components/cards/landing/landingEventsCard.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
-import { mediaurl } from "../../config";
-import { Link } from 'gatsby'
+import { mediaurl } from '../../config';
+import { Link } from 'gatsby';
 //TODO use logo
 export default function EventsCard({ logo, id, name }) {
   return (
-    <Link to={"/event/" + id}>
+    <Link to={'/event/' + id}>
       <div>
         <img
           className='shadow hover:shadow-2xl h-72 w-72 rounded-md object-cover'
           src={mediaurl + logo}
         />
-        <h3 className='p-3'>
-          {name}
-        </h3>
+        <h3 className='p-3'>{name}</h3>
       </div>
     </Link>
   );
