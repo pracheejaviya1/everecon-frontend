5860c96487958380c06c1f4f3076efcd906159ee
Dhruvil Dave <dhruvil.dave@outlook.com>
2021-05-04 15:19:56 +0530

    Test updated

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -4,7 +4,7 @@ context('Actions', () => {
   // https://on.cypress.io/interacting-with-elements
 
   it('.type() - type into a DOM element', () => {
-    cy.visit('http://localhost:8000/signin');
+    cy.visit('http://localhost:8000/Create/Community/updateCommunity');
     cy.get('#header').should('have.class', 'flex');
   });
 
@@ -19,12 +19,22 @@ context('Actions', () => {
   });
 
   it('.type() - type into a DOM element', () => {
-    cy.visit('http://localhost:8000/signin');
+    cy.visit('http://localhost:8000/signup');
     cy.get('#header').should('have.class', 'flex');
   });
 
   it('.type() - type into a DOM element', () => {
-    cy.visit('http://localhost:8000/signup');
+    cy.visit('http://localhost:8000/Create/Community/createCommunityPage1');
+    cy.get('#header').should('have.class', 'flex');
+  });
+
+  it('.type() - type into a DOM element', () => {
+    cy.visit('http://localhost:8000/Create/Community/createCommunityPage2');
+    cy.get('#header').should('have.class', 'flex');
+  });
+
+  it('.type() - type into a DOM element', () => {
+    cy.visit('http://localhost:8000/Create/Community/createCommunityPage3');
     cy.get('#header').should('have.class', 'flex');
   });
 });
