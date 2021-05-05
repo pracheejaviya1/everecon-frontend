4eaffbb792d577cefe668f162bf5ed03d69ef15b
pracheejaviya1 <prachee.j@ahduni.edu.in>
2021-05-05 22:02:47 +0530

    View event css fixed

--- src/components/eventComponents/Desc.tsx
+++ src/components/eventComponents/Desc.tsx
@@ -55,56 +55,107 @@ export default function EventDesc(props: any) {
         <img className='rounded-md' src={mediaurl + props.imageurl} />
         <h2 className='my-4 text-xl font-semibold'>Details</h2>
         <div>
-          <div className='flex flex-row mt-4 w-4/5'>
-            <p className='font-mulish text-base font-light'>
-              {props.description}
+          <div className='flex flex-row mt-6 w-4/5'>
+            <p className='font-inter text-lg font-light'>
+              {props.description} EVENT DECSCRIPTION Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt mollit anim id est laborum.
             </p>
           </div>
-          <div className='flex flex-row mt-12'>
-            <p className='w-36 h-24 text-xl font-mulish'>Address</p>
-            <p className='font-mulish text-base mx-5 font-light'>
-              {props?.Address || 'address'}, {props?.City || 'city'},
+          <hr className='my-4' />
+
+          <div className='flex flex-row mt-6'>
+            <p className='text-xl font-mulish'>Address</p>
+            <p className='font-mulish text-base mx-14 font-light'>
+              {props?.Address || 'address'} {props?.City || 'city'}
               {props?.Country || 'country'}
             </p>
           </div>
-          <div className='flex flex-col mb-12'>
+          <div className='flex flex-row'>
+            <p className=' text-xl font-mulish'>City</p>
+            <p className='font-mulish text-base mx-24 font-light'>
+              {props?.City || 'city'}
+            </p>
+          </div>
+          <div className='flex flex-row'>
+            <p className=' text-xl font-mulish'>Country</p>
+            <p className='font-mulish text-base mx-16 font-light'>
+              {props?.Country || 'country'}
+            </p>
+          </div>
+
+          <div className='flex flex-col mt-6 mb-12'>
             <div className='flex flex-row items-center mt-2'>
-              <label
-                htmlFor='event url'
-                className='mb-1 font-mulish w-24 text-lg mt-1'
-              >
+              <label htmlFor='event url' className='text-xl font-mulish'>
                 Event Link
               </label>
               <a
-                className='rounded-lg mx-5 w-96 font-mulish text-blue-400 text-base font-light'
+                className='rounded-lg mx-10 w-96 font-mulish text-blue-400 text-base font-light'
                 href={props?.URL || 'url'}
               >
                 {props?.URL || 'url'}
               </a>
             </div>
-            <div>
-              <label htmlFor='Links' className='mb-1 font-inter text-lg '>
-                Contact
+            <div className='flex flex-row items-center mt-6'>
+              <label htmlFor='start time' className='text-md w-48 font-mulish'>
+                Start Time
               </label>
-              <div className='flex flex-row mt-2 w-4/5'>
-                <p className='font-mulish text-base font-light'>
-                  {props.description}
-                </p>
-              </div>
+              <p className='rounded-lg mx-5 w-96 font-mulish text-green-500 mx-12 text-lg font-light'>
+                {props?.startTime || 'startTime'}
+              </p>
+              <label htmlFor='end time' className='text-md w-48 font-mulish'>
+                End Time
+              </label>
+              <p className='rounded-lg mx-5 w-96 font-mulish text-green-500 mx-12 text-lg font-light'>
+                {props?.endTime || 'endTIme'}
+              </p>
+            </div>
+            <div className='flex flex-row items-center mt-6'>
+              <label htmlFor='category' className='text-xl font-mulish'>
+                Category
+              </label>
+              <p className='rounded-lg mx-5 w-96 font-mulish text-orange-600 mx-12 text-lg font-light'>
+                {props?.category || 'category'}
+              </p>
+            </div>
+            <hr className='my-4' />
+            <div className='flex flex-row text-indigo-500 mt-4 items-center text-xl font-mulish'>
+              <p>Hosted with</p>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5 mx-2'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
+                  clipRule='evenodd'
+                />
+              </svg>
+              <p>by</p>
+            </div>
+
+            <div className='flex flex-row items-center mt-3'>
+              <p className='rounded-lg w-96 font-mulish text-orange-600 text-lg font-light'>
+                Community Name
+              </p>
+            </div>
+            <div className='flex flex-col items-start mt-3'>
+              <p className='rounded-lg w-96 font-mulish text-orange-600 text-md font-light'>
+                Community Contact email
+              </p>
+              <p className='rounded-lg w-96 font-mulish text-orange-600 text-md font-light'>
+                Community Contact email
+              </p>
             </div>
           </div>
         </div>
-        {/* <h2 className='my-4 text-xl font-semibold'>Details</h2> */}
-
-        {/* <div className='my-2 grid '>
-          <p>City: {props?.City || 'city'}</p>
-          <p>Country: {props?.Country || 'country'}</p>
-          <p>Address: {props?.Address || 'address'}</p>
-          <p>Start Time: {props?.Start || 'starttime'}</p>
-          <p>End Time: {props?.End || 'endtime'}</p>
-          <p>Event URL: {props?.URL || 'url'}</p>
-        </div>
-        <p>{props.description}</p>*/}
       </div>
       <div className='flex flex-col items-center justify-around'>
         <SpeakerBlock />
--- src/components/eventComponents/Speaker.tsx
+++ src/components/eventComponents/Speaker.tsx
@@ -3,11 +3,15 @@ import image from '../../assets/Images/community.jpg';
 
 export default function SpeakerBlock() {
   return (
-    <div className='flex flex-col bg-gray-100 p-4 rounded-lg'>
+    <div className='flex flex-col bg-gray-100 py-6 px-8 rounded-lg'>
       <div className='font-light'>Host - John Doe</div>
-      <img src={image} className='h-16 w-16 rounded-full my-2' />
-      <h1 className='text-xl font-semibold'>Speaker - John Doe</h1>
+      <p className='font-mulsih text-lg font-base'>Speaker</p>
+      <div className='flex flex-row items-center'>
+        <img src={image} className='h-16 w-16 rounded-full my-2 mr-4' />
+        <h1 className='text-xl font-semibold'>John Doe</h1>
+      </div>
       <div className='font-light'>Date</div>
+      <div className='font-light'>Mode</div>
     </div>
   );
 }
