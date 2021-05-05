31b15352cb9b68386ae66de4f1410be1eeb5380a
Kaushal1011 <kaushalpatil10@gmail.com>
2021-05-05 21:26:20 +0530

    media url  update

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,4 +1,4 @@
 export const graphqlurl = 'https://everecon-api.herokuapp.com/graphql/';
-export const mediaurl = 'https://everecon-api.herokuapp.com/';
+export const mediaurl = 'https://res.cloudinary.com/everecon-media/image/upload/v1/';
 // export const graphqlurl = 'http://localhost:8000/graphql/';
 // export const mediaurl = 'http://localhost:8000/media/';
